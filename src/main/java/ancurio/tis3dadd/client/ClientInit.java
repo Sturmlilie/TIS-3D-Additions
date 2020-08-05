a4ec5940a9f4870d3bd8217485791c9bb7ecbdd1
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-08-05 20:10:35 +0200

    Formatting

--- src/main/java/ancurio/tis3dadd/client/ClientInit.java
+++ src/main/java/ancurio/tis3dadd/client/ClientInit.java
@@ -3,9 +3,9 @@ package ancurio.tis3dadd.client;
 import li.cil.tis3d.api.ClientExtInitializer;
 
 public class ClientInit implements ClientExtInitializer {
-	@Override
-	public void onInitializeClient() {
+    @Override
+    public void onInitializeClient() {
 
-		System.out.println("Hello TIS-3D client world!");
-	}
+        System.out.println("Hello TIS-3D client world!");
+    }
 }
