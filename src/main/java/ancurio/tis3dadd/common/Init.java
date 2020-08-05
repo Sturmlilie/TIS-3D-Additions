2f699c883954c2d416a056210656b47896cd99b6
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-08-05 21:07:26 +0200

    Add TwoDigitDisplayModule

--- src/main/java/ancurio/tis3dadd/common/Init.java
+++ src/main/java/ancurio/tis3dadd/common/Init.java
@@ -1,11 +1,36 @@
 package ancurio.tis3dadd.common;
 
+import li.cil.tis3d.api.API;
 import li.cil.tis3d.api.ExtInitializer;
+import li.cil.tis3d.api.ModuleAPI;
+import li.cil.tis3d.api.machine.Casing;
+import li.cil.tis3d.api.machine.Face;
+import li.cil.tis3d.api.module.Module;
+import li.cil.tis3d.api.module.ModuleProvider;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Init implements ExtInitializer {
-	@Override
-	public void onInitialize() {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello TIS-3D common world!");
+        final Identifier moduleId = new Identifier("tis3d-additions", "module_twodigitdisplay");
 
-		System.out.println("Hello TIS-3D common world!");
-	}
+        final Item.Settings settings = new Item.Settings().group(API.itemGroup);
+        final Item moduleItem = new Item(settings);
+        Registry.register(Registry.ITEM, moduleId, moduleItem);
+
+        ModuleAPI.addProvider(new ModuleProvider() {
+            public boolean worksWith(final ItemStack stack, final Casing casing, final Face face) {
+                return stack.getItem() == moduleItem;
+            }
+
+            public Module createModule(final ItemStack stack, final Casing casing, final Face face) {
+                return new TwoDigitDisplayModule(casing, face);
+            }
+        });
+    }
 }
---
+++ src/main/java/ancurio/tis3dadd/common/TwoDigitDisplayModule.java
@@ -0,0 +1,152 @@
+package ancurio.tis3dadd.common;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import li.cil.tis3d.api.API;
+import li.cil.tis3d.api.detail.FontRendererAPI;
+import li.cil.tis3d.api.machine.Casing;
+import li.cil.tis3d.api.machine.Face;
+import li.cil.tis3d.api.machine.Pipe;
+import li.cil.tis3d.api.machine.Port;
+import li.cil.tis3d.api.prefab.module.AbstractModuleWithRotation;
+import li.cil.tis3d.api.util.RenderUtil;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.nbt.CompoundTag;
+
+/**
+ * The TwoDigitDisplayModule will display the lower 8 bit of whichever
+ * value was last read from any of its ports.
+ */
+public final class TwoDigitDisplayModule extends AbstractModuleWithRotation {
+    // --------------------------------------------------------------------- //
+    // Persisted data
+
+    // The value; only lower 8 bit are displayed.
+    private short value;
+
+    // NBT data names.
+    private static final String TAG_VALUE = "value";
+
+    // Data packet types.
+    private static final byte DATA_TYPE_UPDATE = 0;
+
+    // --------------------------------------------------------------------- //
+
+    public TwoDigitDisplayModule(final Casing casing, final Face face) {
+        super(casing, face);
+    }
+
+    // --------------------------------------------------------------------- //
+    // Module
+
+    @Override
+    public void step() {
+        stepInput();
+    }
+
+    @Override
+    public void onDisabled() {
+        // Clear timer on shutdown.
+        value = 0;
+        sendData();
+    }
+
+    @Override
+    public void onData(final ByteBuf data) {
+        value = data.readShort();
+    }
+
+    @Environment(EnvType.CLIENT)
+    @Override
+    public void render(final BlockEntityRenderDispatcher rendererDispatcher, final float partialTicks,
+                       final MatrixStack matrices, final VertexConsumerProvider vcp,
+                       final int light, final int overlay) {
+        if (!getCasing().isEnabled()) {
+            return;
+        }
+
+        matrices.push();
+        rotateForRendering(matrices);
+
+        final int red = value >> 8 & 0xF;
+        final int green = value >> 4 & 0xF;
+        final int blue = value & 0xF;
+        // Maximum: 16.0f
+        final float luminance = 0.2126f * red + 0.7152f * green + 0.0722f * blue;
+        final float threshold = 0.5f * 16;
+        final int textColor = luminance < threshold ? 0xFFFFFFFF : 0xFF000000;
+
+        final String displayString = String.format("%02X", value & 0xFF);
+        final VertexConsumer vcFont = API.fontRendererAPI.chooseVertexConsumer(API.Font.NormalFont, vcp);
+
+        final int width = displayString.length() * API.fontRendererAPI.getCharWidth();
+        final int height = API.fontRendererAPI.getCharHeight();
+
+        matrices.push();
+        matrices.translate(0.1f, 0.1f, 0);
+        matrices.scale(1 / 20f, 1 / 20f, 1);
+
+        API.fontRendererAPI.drawString(API.Font.NormalFont, matrices.peek(), vcFont, light, overlay,
+                                textColor, displayString, displayString.length());
+
+        matrices.pop();
+        matrices.translate(0f, 0f, 0.005f / 2);
+
+        final VertexConsumer vc = vcp.getBuffer(RenderLayer.getSolid());
+        final int color = 0xFF << 24 | red << 20 | green << 12 | blue << 4;
+
+        final float px = 1.0f / 32.0f;
+        RenderUtil.drawColorQuad(matrices.peek(), vc, px, px, 1-px*2, 1-px*2,
+                                 color, RenderUtil.maxLight, overlay);
+
+        matrices.pop();
+    }
+
+    @Override
+    public void readFromNBT(final CompoundTag nbt) {
+        super.readFromNBT(nbt);
+
+        value = nbt.getShort(TAG_VALUE);
+    }
+
+    @Override
+    public void writeToNBT(final CompoundTag nbt) {
+        super.writeToNBT(nbt);
+
+        nbt.putShort(TAG_VALUE, value);
+    }
+
+    // --------------------------------------------------------------------- //
+
+    /**
+     * Update the inputs of the module.
+     */
+    private void stepInput() {
+        for (final Port port : Port.VALUES) {
+            // Continuously read from all ports.
+            final Pipe receivingPipe = getCasing().getReceivingPipe(getFace(), port);
+            if (!receivingPipe.isReading()) {
+                receivingPipe.beginRead();
+            }
+            if (receivingPipe.canTransfer()) {
+                // Set the value.
+                value = receivingPipe.read();
+                sendData();
+            }
+        }
+    }
+
+    private void sendData() {
+        final ByteBuf data = Unpooled.buffer();
+        data.writeShort(value);
+        getCasing().sendData(getFace(), data, DATA_TYPE_UPDATE);
+    }
+}
